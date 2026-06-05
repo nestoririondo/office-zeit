9e5b4faea2c9189198c2e6fbfbd01529dbb6e7fe
Néstor Iriondo <nestor.iriondo@murrelektronik.de>
2026-06-05 12:38:06 +0200

    feat: add gap between header elements for improved layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Header({ person, color }: Props) {
   return (
-    <header className="flex items-center justify-between pb-6">
+    <header className="flex items-center justify-between pb-6 gap-6">
       <h1 className="font-mono font-bold text-2xl">Wann kommst du ins Office?</h1>
       <div className="flex items-center gap-6">
         <DarkModeToggle />
